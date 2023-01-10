f1e1d417f5720ef86f0ef682016091de418b74e4
Teodor <teodor2@abv.bg>
2023-01-10 22:36:35 +0200

    register user ready

--- pages/api/controllers/user.tsx
+++ pages/api/controllers/user.tsx
@@ -2,7 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Connect from "../../../utils/mongoDBMongooseConnection";
 import User from "../../../models/user";
 type Data = {
-  name: string;
+  username: string;
+  password: string;
+  rePassword?: string;
   email: string;
 };
 
@@ -10,8 +12,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { username, password, repassword, email } = req.body;
-  await Connect();
-  const user = await User.create<Data>(req.body);
-  res.json(user);
+  if (req.method === "POST") {
+    const { username, password, rePassword, email } = req.body;
+    if (
+      username &&
+      password &&
+      rePassword &&
+      email &&
+      password === rePassword
+    ) {
+      try {
+        const data = {
+          username,
+          password,
+          email,
+        };
+        await Connect();
+        const user = await User.create<Data>(data);
+        res.json(user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
 }
