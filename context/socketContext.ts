907e55eb6210af02e024919ee524abcef34f5b70
Teodor <teodor2@abv.bg>
2023-04-28 00:20:54 +0300

    vercel update connection

--- context/socketContext.ts
+++ context/socketContext.ts
@@ -1,8 +1,4 @@
 import React from "react";
-import io, { Socket } from "socket.io-client";
-fetch("/api/socket");
-export const socket = io();
-socket.on("connect", () => {
-  console.log("connected")
-});
+import { Socket } from "socket.io-client";
+
 export const SocketContext = React.createContext<undefined | Socket>(undefined);
